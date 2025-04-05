27715a8658813fdd0bcbbd20d20af84aedf11af1
Thiago Sosa <84207498+Sosat-pixel@users.noreply.github.com>
2025-04-05 13:05:36 -0300

    Correción ej3

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,32 +1,47 @@
 public class Ejercicio3 {
     public static void main(String[] arg) {
-
+        //Definimos los arrays junto al array aux
         int vec1[] = {2, 6, 4, 5, 8, 9, 6, 4};
         int vec2[] = {10, 2, 5, 10, 4, 7, 5, 6};
         int vecAux[] = new int[8];
 
+        //Imprimimos los arrays iniciales
+        //Vec1
         System.out.println("PreCambio");
         for (int i = 0; i < vec1.length; i++)
         {
                 System.out.print(vec1[i]);
                 System.out.print(" - ");
-                System.out.print(vec2[i]);
-                System.out.print(" - ");
         }
-            for (int i = 0; i < vec1.length; i++)
-            {
-                vec1[i] = vecAux[i];
-                vec2[i] = vec1[i];
-                vec2[i] = vecAux[i];
-            }
-            System.out.println("PostCambio");
-            for (int i = 0; i < vec1.length; i++)
-            {
-                System.out.print(vec1[i]);
-                System.out.print(" - ");
-                System.out.print(vec2[i]);
-                System.out.print(" - ");
-            }
+        System.out.println("|");
+        //Vec2
+        for (int i = 0; i < vec2.length; i++)
+        {
+            System.out.print(vec2[i]);
+            System.out.print(" - ");
+        }
+        //Intercambio
+        for (int i = 0; i < vec1.length; i++)
+        {
+            vecAux[i] = vec1[i];
+            vec1[i] = vec2[i];
+            vec2[i] = vecAux[i];
+        }
+        //Vec1
+        System.out.println("");
+        System.out.println("PostCambio");
+        for (int i = 0; i < vec1.length; i++)
+        {
+            System.out.print(vec1[i]);
+            System.out.print(" - ");
+        }
+        System.out.println("|");
+        //Vec2
+        for (int i = 0; i < vec2.length; i++)
+        {
+            System.out.print(vec2[i]);
+            System.out.print(" - ");
+        }
 
 
     }
