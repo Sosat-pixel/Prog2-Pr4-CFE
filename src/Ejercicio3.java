029e744a26237fdd876d96501b90eaacd61cce85
Thiago Sosa <84207498+Sosat-pixel@users.noreply.github.com>
2025-04-04 23:00:02 -0300

    Ejercicios 1 - 3 prácticos

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -3,15 +3,13 @@ public class Ejercicio3 {
 
         int vec1[] = {2, 6, 4, 5, 8, 9, 6, 4};
         int vec2[] = {10, 2, 5, 10, 4, 7, 5, 6};
-        int vecAux[] = {0, 0, 0, 0, 0, 0, 0, 0};
+        int vecAux[] = new int[8];
 
         System.out.println("PreCambio");
         for (int i = 0; i < vec1.length; i++)
         {
-                System.out.println("Vector1");
                 System.out.print(vec1[i]);
                 System.out.print(" - ");
-                System.out.println("Vector2");
                 System.out.print(vec2[i]);
                 System.out.print(" - ");
         }
